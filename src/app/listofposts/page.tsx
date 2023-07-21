6f49fb9e881fbfc946d7124ff314aed61868634d
mangala-karunarathne <mangalaicc@gmail.com>
2023-07-21 11:26:41 +0530

    CHG | Fetch User Details

--- src/app/listofposts/page.tsx
+++ src/app/listofposts/page.tsx
@@ -3,16 +3,26 @@ const getPostsData = async () => {
   return res.json();
 };
 
+const getUsersData = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  return res.json();
+};
 
 export default async function ListOfPosts() {
-  const posts = await getPostsData();
-  console.log(posts);
-  
+  const [posts, users] = await Promise.all([getPostsData(), getUsersData()]);
+
   return (
     <div>
-      {posts.map((post:any)=>{
-        return <p>{post.title}</p>;
-      })}
+      <div>
+        {posts.map((post: any) => {
+          return <p>{post.title}</p>;
+        })}
+      </div>
+      <div>
+        {users.map((user: any) => {
+          return <p>{user.name}</p>;
+        })}
+      </div>
     </div>
   );
 }
