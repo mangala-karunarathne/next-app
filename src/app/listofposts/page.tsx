df542376e394483bc574159ce75b0e45c71c3a55
mangala-karunarathne <mangalaicc@gmail.com>
2023-07-21 11:52:02 +0530

    CHG | Remove the cache and get random data when refreshing

--- src/app/listofposts/page.tsx
+++ src/app/listofposts/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 const getPostsData = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   return res.json();
@@ -7,12 +9,19 @@ const getUsersData = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   return res.json();
 };
+const getDogData = async () => {
+  const res = await fetch("https://dog.ceo/api/breeds/image/random", {
+    cache: "no-cache"
+  });
+  return res.json();
+};
 
 export default async function ListOfPosts() {
-  const [posts, users] = await Promise.all([getPostsData(), getUsersData()]);
+  const [posts, users,dog] = await Promise.all([getPostsData(), getUsersData(), getDogData()]);
 
   return (
     <div>
+      <Image src={dog.message} alt="dog" width={300} height={300}/>
       <div>
         {posts.map((post: any) => {
           return <p>{post.title}</p>;
