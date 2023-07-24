3ad6f0609580466721b3340da37e3ce35c06f6dc
mangala-karunarathne <mangalaicc@gmail.com>
2023-07-24 12:36:57 +0530

    CHG | Add Loading until data fetch

--- src/app/listofposts/page.tsx
+++ src/app/listofposts/page.tsx
@@ -1,39 +1,28 @@
 import Image from 'next/image';
 
-const getPostsData = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  return res.json();
-};
+// const getPostsData = async () => {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+//   return res.json();
+// };
 
-const getUsersData = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  return res.json();
-};
+// const getUsersData = async () => {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/users");
+//   return res.json();
+// };
+
 const getDogData = async () => {
   const res = await fetch("https://dog.ceo/api/breeds/image/random", {
-    next: {
-      revalidate: 10,
-    },
+    cache: "no-store",
   });
   return res.json();
 };
 
 export default async function ListOfPosts() {
-  const [posts, users,dog] = await Promise.all([getPostsData(), getUsersData(), getDogData()]);
+  const dog = await getDogData();
 
   return (
     <div>
       <Image src={dog.message} alt="dog" width={300} height={300}/>
-      <div>
-        {posts.map((post: any) => {
-          return <p>{post.title}</p>;
-        })}
-      </div>
-      <div>
-        {users.map((user: any) => {
-          return <p>{user.name}</p>;
-        })}
-      </div>
     </div>
   );
 }
